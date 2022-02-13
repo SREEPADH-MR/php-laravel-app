75c162b18475201fecb0d36a31ae82d6a224896b
Sreepadh-flotillatechs <sreepadhraju@gmail.com>
2022-02-13 23:05:37 +0530

    favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>PHP LARAVEL APP</title>
+        <link rel="icon" type="image/x-icon" href="https://www.google.com/favicon.ico">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
